047290f72dd3a3cf34d277868174fac8645f6986
Eric Dugre <ericd@kentico.com>
2021-05-05 10:30:52 -0400

    Rating view

--- Xperience/XperienceAttachmentDownloader.cs
+++ Xperience/XperienceAttachmentDownloader.cs
@@ -1,4 +1,5 @@
-﻿using CMS.DocumentEngine;
+﻿using CMS.Base;
+using CMS.DocumentEngine;
 using Statiq.Common;
 using System.Collections.Generic;
 using System.IO;
@@ -13,27 +14,32 @@ namespace StatiqGenerator
 
         }
 
+        #pragma warning disable 1998
         protected override async Task<IEnumerable<IDocument>> ExecuteInputAsync(IDocument input, IExecutionContext context)
         {
             var node = XperienceDocumentConverter.ToTreeNode<TreeNode>(input);
             foreach (var attachment in node.AllAttachments)
             {
-                await Task.Run(() => DownloadAttachment(attachment));
+                DownloadAttachment(attachment);
             }
 
             return input.Yield();
         }
+        #pragma warning restore 1998
 
         private void DownloadAttachment(DocumentAttachment attachment)
         {
             var fileName = $"input{StatiqHelper.AttachmentPath}/{attachment.AttachmentName}";
             if (!File.Exists(fileName))
             {
-                var binary = AttachmentBinaryHelper.GetAttachmentBinary(attachment);
-                BinaryWriter writer = new BinaryWriter(new FileStream(fileName, FileMode.Create));
-                writer.Write(binary);
-                writer.Flush();
-                writer.Close();
+                var thread = new CMSThread(() => {
+                    var binary = AttachmentBinaryHelper.GetAttachmentBinary(attachment);
+                    BinaryWriter writer = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+                    writer.Write(binary);
+                    writer.Flush();
+                    writer.Close();
+                });
+                thread.Start();
             }
         }
     }
