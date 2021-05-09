ee5e37e9f12044c2c4ab9c0117af31431bbb382d
Eric Dugre <ericd@kentico.com>
2021-05-09 18:52:40 -0400

    Try deploying to GH Pages

--- Program.cs
+++ Program.cs
@@ -29,9 +29,11 @@ namespace StatiqGenerator
                 .AddPipeline<AuthorPipeline>()
                 .AddPipeline<ContactPipeline>()
                 .AddPipeline("Assets", outputModules: new IModule[] { new CopyFiles("assets/**") })
-                .DeployToNetlify(
-                    Environment.GetEnvironmentVariable("NetlifySiteId"),
-                    Environment.GetEnvironmentVariable("NetlifyAccessToken")
+                .DeployToGitHubPages(
+                    "kentico-ericd",
+                    "xperience-statiq-sample",
+                    Config.FromSetting<string>("GITHUB_TOKEN"),
+                    "master"
                 )
                 .RunAsync();
         }
