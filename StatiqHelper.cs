367428a231655ef86f87f8da989d4696c73fd024
Eric Dugre <ericd@kentico.com>
2021-05-13 10:00:52 -0400

    Fix attachment path

--- StatiqHelper.cs
+++ StatiqHelper.cs
@@ -7,7 +7,7 @@ namespace StatiqGenerator
     {
         public const string BookPath = "books";
         public const string AuthorPath = "authors";
-        public const string AttachmentPath = "assets/attachments";
+        public const string AttachmentPath = "books/attachments";
         public const string AdministrationURL = "https://ericd-statiq.azurewebsites.net";
 
         public static string GetBookUrl(Book book)
